12ceb77c99d67fa42c55925cfa5e7649e2dc4b9e
Ushiyariv.Timur <uti2000@yandex.ru>
2023-02-26 10:41:24 +0300

    Added fourth task

---
+++ Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿// 4.Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран 
+// выводя индексы соответствующего элемента
+
+int[,,] array3D = new int[3, 3, 3];
+FillArray(array3D);
+PrintIndex(array3D);
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
